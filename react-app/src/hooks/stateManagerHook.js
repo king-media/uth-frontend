d0d99beefb3fd20a412b06d65a4b9be65e744fbf
Darrell Washington <darrellwjr.com@gmail.com>
2021-10-13 16:59:58 -0400

    code clean up

--- react-app/src/hooks/stateManagerHook.js
+++ react-app/src/hooks/stateManagerHook.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 
 export default function useStateManager(initialState) {
     const [internalState, setInternalState] = useState(initialState)
--- react-app/src/pages/TodosPage.js
+++ react-app/src/pages/TodosPage.js
@@ -1,4 +1,4 @@
-import React, {useState, useContext, useEffect} from 'react'
+import React, { useState, useContext } from 'react'
 import TodosList from '../components/TodosList'
 import { TodosStateContext } from '../state'
 
